e1df0464529140a369663472d9dda1e859640d30
rafareyesRomida <100431342+rafareyesRomida@users.noreply.github.com>
2022-03-01 13:21:19 -0600

    Actualizacion refaccionesid.php

--- refaccionesid.php
+++ refaccionesid.php
@@ -1,42 +1,105 @@
 <?php include("template/cabecera.php"); ?>
 
-<div class="progress">
-  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
-</div>
+<?php
 
-<?php 
-include("administrador/config/bd.php");
+$txtID = (isset($_POST['txtID'])) ? $_POST['txtID'] : "";
+$txtRefaccion = (isset($_POST['txtRefaccion'])) ? $_POST['txtRefaccion'] : "";
+$txtCodigo = (isset($_POST['txtCodigo'])) ? $_POST['txtCodigo'] : "";
+$txtExistencia = (isset($_POST['txtExistencia'])) ? $_POST['txtExistencia'] : "";
 $txtId_Tractos = (isset($_POST['txtId_Tractos'])) ? $_POST['txtId_Tractos'] : "";
+$txtImg = (isset($_FILES['txtImg']['name'])) ? $_FILES['txtImg']['name'] : "";
 $accion = (isset($_POST['accion'])) ? $_POST['accion'] : "";
-
-$sentenciaSQL = $conexion->prepare("SELECT refacciones.id, refacciones.refaccion, refacciones.codigo, refacciones.existencia, refacciones.id_tractos, refacciones.img  FROM refacciones  WHERE id_tractos='$txtId_Tractos'");
-$sentenciaSQL->execute();
-$listaRefacciones =$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
 
 
+      <?php
+      include("administrador/config/bd.php");
+      $sentenciaSQL = $conexion->prepare("SELECT * FROM refacciones WHERE id_tractos=:Id_Tractos");
+      $sentenciaSQL->bindParam(':Id_Tractos', $txtId_Tractos);
+      $sentenciaSQL->execute();
+      $listaRefacciones =$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+      ?>
+      
 
-<?php foreach($listaRefacciones as $Refaccion) {?>
-    
+        <?php
+       /* header("location:refaccionesid.php");
+        break;
+        
+
+}*/?>
+
+
+
+
+
 
 <div class="col-md-4">
+    Agregar Refacciones
     <div class="card">
-        <img class="card-img-top" src="./img/<?php echo $Refaccion['img']; ?>" alt="">
-        <div class="card-body">
-            <h4 class="card-title"><?php echo $Refaccion['refaccion']; ?></h4>
-            <td> <?php echo $Refaccion['codigo']; ?></td>
-            <h6 class="card-title">Existencias: <?php echo $Refaccion['existencia']; ?></h6>
-
-            
+        <div class="card-header">
+            Descripcion de las refacciones
         </div>
-    </div> 
+        <div class="card-body">
+            <form method="POST" enctype="multipart/form-data">
+
+
+                
+
+                <div class="form-group">
+                    <label for="txtId_Tractos">Id_Tracto:</label>
+                    <input type="text" required class="form-control" value="1" name="txtId_Tractos" id="txtId_Tractos" placeholder="id_tractos">
+                </div>
+                
+
+                
+                <div class="btn-group" role="group" aria-label="">
+                    <button type="submit" name="accion"  value="Buscar" class="btn btn-info">Buscar</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+
+</div>
+<div class="col-md-7">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Refaccion</th>
+                <th>Codigo</th>
+                <th>Existencia</th>
+                <th>Id_Tractos</th>
+                <th>Imagen</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <?php foreach ($listaRefacciones as $Refaccion) { ?>
+
+
+                <tr>
+                    <td><?php echo $Refaccion['id']; ?></td>
+                    <td><?php echo $Refaccion['refaccion']; ?></td>
+                    <td><?php echo $Refaccion['codigo']; ?></td>
+                    <td><?php echo $Refaccion['existencia']; ?></td>
+                    <td><?php echo $Refaccion['id_tractos']; ?></td>
+                    <td>
+                        <img class="img-thumbail rounded" src="img/<?php echo $Refaccion['img']; ?>" width="170" alt="" srcset="">
+                    </td>
+                        <form method="post">
+
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 </div>
 
-<?php } ?>
-<div class="progress">
-  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
-</div>
+
+
 
 <?php include("template/pie.php"); ?>
