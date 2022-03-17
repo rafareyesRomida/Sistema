443c811684045dd5002bf8d01c975fc081929ef1
rafareyesRomida <100431342+rafareyesRomida@users.noreply.github.com>
2022-03-17 17:34:46 -0600

    actualizacion de datos de la empresa en pie.php

--- template/pie.php
+++ template/pie.php
@@ -1,4 +1,75 @@
 </div>
-    </div>
+        </div>
 </body>
 </html>
+<footer class="text-center text-lg-start text-primary">
+
+  <!-- Section: Links  -->
+  <br>
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>RMD Terrestre
+          </h6>
+          <p>
+          Comprometida a superar de forma integra y eficaz los requerimientos de 
+          trasportación de mercancía refrigerada, consolidación y distribución 
+          de mercancías en general de sus clientes.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+          Enlaces útiles
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Atención a clientes</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ajustes</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ayuda</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contacto
+          </h6>
+          <p><i class="fas fa-home me-3"></i> Limón, 54600 San Mateo Xoloc, Méx.</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            logistica@romida.com.mx
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +52 5557191596</p>
+          <p><i class="fas fa-print me-3"></i> +52 5557192833</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://romida.com.mx/transporte/">RMD</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
