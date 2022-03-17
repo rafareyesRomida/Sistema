6a2e3fd8b9e406fbbed2cdb3cd4c6682e85aff58
rafareyesRomida <100431342+rafareyesRomida@users.noreply.github.com>
2022-03-17 17:31:40 -0600

    Actualizacion con kilometrajes servicios.php

--- servicios.php
+++ servicios.php
@@ -1,53 +1,54 @@
 <?php include("template/cabecera.php"); ?>
 <h1>SERVICIOS</h1>
-<div class="alert alert-dismissible alert-info">
-  <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-  <h4 class="alert-heading">SERVICIOS</h4>
-  <p class="mb-2">Aqui se mostraran los ultimos servicios de cada una de las unidades asi como en que fecha se tienen que realizar los proximos servicios<a href="#" class="alert-link"></a>.</p>
-</div>
+  </div>
 
 <?php
 $txtID = (isset($_POST['txtID'])) ? $_POST['txtID'] : "";
 $txtMarca = (isset($_POST['txtMarca'])) ? $_POST['txtMarca'] : "";
 $txtPlacas = (isset($_POST['txtPlacas'])) ? $_POST['txtPlacas'] : "";
+$txtid_tractos=(isset($_POST['id_tractos'])) ? $_POST['id_tractos'] : "";
 include("administrador/config/bd.php");
 
-$sentenciaSQL = $conexion->prepare("SELECT id_tractos, Marca, Placas FROM tractos");
+$sentenciaSQL = $conexion->prepare("SELECT tractos.id_tractos, tractos.Marca, tractos.Placas, tractos.economico, MAX(diesel.km), viajes.id_tractos, viajes.id_Viajes FROM viajes INNER JOIN diesel ON diesel.id_Viajes=viajes.id_Viajes INNER JOIN tractos ON viajes.id_tractos=tractos.id_tractos WHERE tractos.id_tractos='2'");
 $sentenciaSQL->execute();
-$listaServicios = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+$listaKm = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+
+
 ?>
-
-
-
 <table class="table table-hover">
   <thead>
     <tr>
-      <th scope="col">Id</th>
+    <th scope="col">Id</th>
       <th scope="col">Unidad</th>
+      <th scope="col">Economico</th>
       <th scope="col">Placas</th>
-      <th scope="col">Ultimo servicio</th>
-      <th scope="col">Proximo servicio</th>
+      <th scope="col">Kilometros por servicio</th>
     </tr>
   </thead>
+  
   <tbody>
-  <?php foreach ($listaServicios as $Servicio) { ?>
+  <?php foreach ($listaKm as $km) { ?>
+    <tr>
+    <td><?php echo $km['id_tractos']; ?></td>
+    <td><?php echo $km['Marca']; ?></td>
+    <td><?php echo $km['Placas']?></td>
+    <td><?php echo $km['economico']; ?></td>
+    <td><?php echo $km['MAX(diesel.km)']; ?></td>
+    
 
-
-<tr>
-    <td><?php echo $Servicio['id_tractos']; ?></td>
-    <td><?php echo $Servicio['Marca']; ?></td>
-    <td><?php echo $Servicio['Placas']; ?></td>
   
     <td>
         <form method="post">
 
-            <input type="hidden" name="txtID" id="txtID" value="<?php echo $Servicio['id_tractos']; ?>" />
+            <input type="text" name="txtID" id="txtID" value="<?php echo $Servicio['id_tractos']; ?>" />
 
         </form>
     </td>
 </tr>
 <?php } ?>
-</tbody>
+
+
+  </tbody>
 </table>
 
 
